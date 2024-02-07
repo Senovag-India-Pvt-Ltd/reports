e1a269f6535253a2349bb36f33e9cc7b0235561a
swathivs810 <152892551+swathivs810@users.noreply.github.com>
2024-02-07 11:05:58 +0530

    report chnages

--- src/main/java/com/sericulture/controller/ReportsController.java
+++ src/main/java/com/sericulture/controller/ReportsController.java
@@ -509,12 +509,14 @@ public class ReportsController {
             }
             if(apiResponse.content.getLotSoldOutAmount().equals("0.0")){
                 apiResponse.content.setLotSoldOutAmount("");
+            }else{
+                apiResponse.content.setLotSoldOutAmount(String.valueOf(Double.parseDouble(apiResponse.content.getTotalamount()) - apiResponse.content.getFarmerMarketFee()));
             }
             if(apiResponse.content.getFeespaid().equals("0.0+0.0=0.0")){
                 apiResponse.content.setFeespaid("");
             }
             if(!apiResponse.content.getBidAmount().equals("")) {
-                apiResponse.content.setReeleramount(String.valueOf(Double.parseDouble(apiResponse.content.getBidAmount()) + apiResponse.content.getReelerMarketFee()));
+                apiResponse.content.setReeleramount(String.valueOf(Double.parseDouble(apiResponse.content.getTotalamount()) + apiResponse.content.getReelerMarketFee()));
             }else{
                 apiResponse.content.setReeleramount("");
             }
