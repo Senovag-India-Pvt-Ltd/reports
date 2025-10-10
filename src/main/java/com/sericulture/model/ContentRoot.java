b77e2677530d5002d3682cb0bfe0f41bd013a424
Chetanshet36 <chetanshet36@gmail.com>
2025-10-10 13:50:10 +0530

    Float

--- src/main/java/com/sericulture/model/ContentRoot.java
+++ src/main/java/com/sericulture/model/ContentRoot.java
@@ -1,9 +1,16 @@
 package com.sericulture.model;
 
-import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ContentRoot {
     public Content content;
     public ArrayList<Object> errorMessages;
     public int errorCode;
+    private Integer totalRecords;
 }
