676d20189c62268db54bafcc2dffcda19823f17b
Chetanshet36 <chetanshet36@gmail.com>
2025-11-29 10:31:46 +0530

    Sanction Order

--- src/main/java/com/sericulture/controller/ApiService.java
+++ src/main/java/com/sericulture/controller/ApiService.java
@@ -440,9 +440,6 @@ public class ApiService {
         //return apiResponse;
     }
     public AcknowledgementResponse fetchData(ApplicationFormPrintRequest requestDto) throws JsonProcessingException {
-        // Make a GET request to the API endpoint
-
-//        String finalapiurl = "http://localhost:8013/dbt/v1/" + "service/acknowledgementReceipt";
 
         String finalapiurl = dbtApiUrl +"service/pmksyAcknowledgement";
 
@@ -770,7 +767,6 @@ public class ApiService {
 
     public WorkOrderReportResponse fetchDataApi(WorkOrderPrintRequest requestDto) throws JsonProcessingException {
         // Make a GET request to the API endpoint
-//        String finalapiurl = "http://localhost:8013/dbt/v1/" + "service/workOrderGeneration";
         String finalapiurl = dbtApiUrl +"service/workOrderGeneration";
 
 //        String finalapiurl ="http://localhost:8013/dbt/v1/" + "service/workOrderGeneration";
@@ -795,7 +791,30 @@ public class ApiService {
 
     public WorkOrderReportResponse fetchDataApiWorkOrder(WorkOrderPrintRequest requestDto) throws JsonProcessingException {
         // Make a GET request to the API endpoint
+        String finalapiurl =dbtApiUrl +"service/workOrder";
+
 //        String finalapiurl ="http://localhost:8013/dbt/v1/" + "service/workOrder";
+
+        // Define the request headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.setBearerAuth(Util.getTokenData());
+
+        HttpEntity<WorkOrderPrintRequest> requestEntity = new HttpEntity<>(requestDto, headers);
+        WorkOrderGenerationReportResponse response = new WorkOrderGenerationReportResponse();
+        String response1=        restTemplate.postForObject(finalapiurl,requestEntity, String.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        WorkOrderReportResponse response2 = objectMapper.readValue(response1, WorkOrderReportResponse.class);
+
+        return response2;
+        // Process the API response as needed
+        //return apiResponse;
+    }
+
+    public WorkOrderReportResponse fetchDataApiWorkOrderHRUReelingShed(WorkOrderPrintRequest requestDto) throws JsonProcessingException {
+        // Make a GET request to the API endpoint
         String finalapiurl =dbtApiUrl +"service/workOrder";
 
 //        String finalapiurl ="http://localhost:8013/dbt/v1/" + "service/workOrder";
