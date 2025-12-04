089c4fed823fff0f614d7583f4a93868cae0399c
Chetanshet36 <chetanshet36@gmail.com>
2025-12-04 11:56:39 +0530

    Sanction Order

--- src/main/java/com/sericulture/controller/ApiService.java
+++ src/main/java/com/sericulture/controller/ApiService.java
@@ -278,28 +278,6 @@ public class ApiService {
 
 
 
-    public SanctionOrder fetchDataFromBonus(CheckInspectionStatusRequest requestDto) throws JsonProcessingException {
-        // Make a GET request to the API endpoint
-        String finalapiurl = dbtApiUrl +"service/getSanctionOrderForK2";
-//        String finalapiurl = "http://localhost:8013/dbt/v1/service/getSanctionOrderForK2";
-
-        // Define the request headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.setBearerAuth(Util.getTokenData());
-
-        HttpEntity<CheckInspectionStatusRequest> requestEntity = new HttpEntity<>(requestDto, headers);
-        SanctionOrderResponse response = new SanctionOrderResponse();
-        String response1=        restTemplate.postForObject(finalapiurl,requestEntity, String.class);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        SanctionOrder response2 = objectMapper.readValue(response1, SanctionOrder.class);
-
-        return response2;
-        // Process the API response as needed
-        //return apiResponse;
-    }
 
     public SeedMarket fetchDataFromPermit(LotStatusSeedMarketRequest requestDto) throws JsonProcessingException {
         // Make a GET request to the API endpoint
@@ -841,30 +819,6 @@ public class ApiService {
 
         ObjectMapper objectMapper = new ObjectMapper();
         WorkOrderReportResponse response2 = objectMapper.readValue(response1, WorkOrderReportResponse.class);
-
-        return response2;
-        // Process the API response as needed
-        //return apiResponse;
-    }
-
-    public SanctionOrder fetchDataApiWorkOrderHRUReelingShed(CheckInspectionStatusRequest requestDto) throws JsonProcessingException {
-        // Make a GET request to the API endpoint
-        String finalapiurl =dbtApiUrl +"service/workOrder";
-
-//        String finalapiurl ="http://localhost:8013/dbt/v1/" + "service/workOrder";
-
-        // Define the request headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.setBearerAuth(Util.getTokenData());
-
-        HttpEntity<CheckInspectionStatusRequest> requestEntity = new HttpEntity<>(requestDto, headers);
-        WorkOrderGenerationReportResponse response = new WorkOrderGenerationReportResponse();
-        String response1=        restTemplate.postForObject(finalapiurl,requestEntity, String.class);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        SanctionOrder response2 = objectMapper.readValue(response1, SanctionOrder.class);
 
         return response2;
         // Process the API response as needed
