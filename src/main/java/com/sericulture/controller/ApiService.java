4aa844b31bfa4f392af2a58ccf286de4e69f9989
Chetanshet36 <chetanshet36@gmail.com>
2026-03-06 21:35:00 +0530

    Reeling Shed selection

--- src/main/java/com/sericulture/controller/ApiService.java
+++ src/main/java/com/sericulture/controller/ApiService.java
@@ -303,6 +303,27 @@ public class ApiService {
         return objectMapper.readValue(responseJson, SanctionOrder.class);
     }
 
+    public SanctionOrder fetchDataFromPsfaReelingShedSelection(CheckInspectionStatusRequest requestDto)
+            throws JsonProcessingException {
+
+        String finalapiurl = dbtApiUrl + "sanctionOrderWorkOrderAcknowledgement/getReelingShedWorkOrderDetails";
+
+//                String finalapiurl = "http://localhost:8013/dbt/v1/sanctionOrderWorkOrderAcknowledgement/getReelingShedWorkOrderDetails";
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.setBearerAuth(Util.getTokenData());
+
+        HttpEntity<CheckInspectionStatusRequest> requestEntity = new HttpEntity<>(requestDto, headers);
+
+        String responseJson = restTemplate.postForObject(finalapiurl, requestEntity, String.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(responseJson, SanctionOrder.class);
+    }
+
 
 
 
