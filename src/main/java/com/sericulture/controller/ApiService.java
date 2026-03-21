fe60db84fbec0582a5afa592d23201ddf5d87f77
Chetanshet36 <chetanshet36@gmail.com>
2026-03-21 17:17:35 +0530

    Endpoints Mapping

--- src/main/java/com/sericulture/controller/ApiService.java
+++ src/main/java/com/sericulture/controller/ApiService.java
@@ -324,6 +324,27 @@ public class ApiService {
         return objectMapper.readValue(responseJson, SanctionOrder.class);
     }
 
+    public SanctionOrder fetchDataFromRearingEquipmentSS(CheckInspectionStatusRequest requestDto)
+            throws JsonProcessingException {
+
+        String finalapiurl = dbtApiUrl + "sanctionOrderWorkOrderAcknowledgement/getRearingEquipmentSilkSamagraSanctionDetails";
+
+//                String finalapiurl = "http://localhost:8013/dbt/v1/sanctionOrderWorkOrderAcknowledgement/getRearingEquipmentSilkSamagraSanctionDetails";
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.setBearerAuth(Util.getTokenData());
+
+        HttpEntity<CheckInspectionStatusRequest> requestEntity = new HttpEntity<>(requestDto, headers);
+
+        String responseJson = restTemplate.postForObject(finalapiurl, requestEntity, String.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(responseJson, SanctionOrder.class);
+    }
+
     public SanctionOrder fetchDataFromPsfaReelingShedSelection(CheckInspectionStatusRequest requestDto)
             throws JsonProcessingException {
 
