1b80720c1b02aa4e68a437144ae45a12b0608a80
Chetanshet36 <chetanshet36@gmail.com>
2026-03-24 22:11:00 +0530

    CRC endpoint Called here

--- src/main/java/com/sericulture/controller/ApiService.java
+++ src/main/java/com/sericulture/controller/ApiService.java
@@ -345,6 +345,27 @@ public class ApiService {
         return objectMapper.readValue(responseJson, SanctionOrder.class);
     }
 
+    public SanctionOrder fetchDataFromPrivateChawkiSanction(CheckInspectionStatusRequest requestDto)
+            throws JsonProcessingException {
+
+        String finalapiurl = dbtApiUrl + "sanctionOrderWorkOrderAcknowledgement/getRegisteredPrivateChawkiSanctionDetails";
+
+//                String finalapiurl = "http://localhost:8013/dbt/v1/sanctionOrderWorkOrderAcknowledgement/getRegisteredPrivateChawkiSanctionDetails";
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.setBearerAuth(Util.getTokenData());
+
+        HttpEntity<CheckInspectionStatusRequest> requestEntity = new HttpEntity<>(requestDto, headers);
+
+        String responseJson = restTemplate.postForObject(finalapiurl, requestEntity, String.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(responseJson, SanctionOrder.class);
+    }
+
     public SanctionOrder fetchDataFromPsfaReelingShedSelection(CheckInspectionStatusRequest requestDto)
             throws JsonProcessingException {
 
